a33e0f8211e060a1e5234696b446e8b5ac86835c
CooperDon <tute_gcyue@163.com>
2020-10-21 13:39:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>导航栏 | <router-link to="/">Frank</router-link>
+    | <router-link to="/xxx">Frank2</router-link>
+</div>
+<hr />
+<router-view></router-view>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+    name: 'App',
+}
+</script>
---
+++ src/components/Frank2.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    Frank2
+</div>
+</template>
